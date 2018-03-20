d46de2e454e0da86662f0b7b293ee34e96e18e43
Andres Galvez <pocyxDesigner@hotmail.com>
2018-03-20 13:25:28 +0100

    displaying a list

--- FutureConnections/src/app/clients/clients.component.ts
+++ FutureConnections/src/app/clients/clients.component.ts
@@ -1,18 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { Client } from '../client';
+import { CLIENTS } from '../mock-clients';
+import { checkAndUpdateDirectiveInline } from '@angular/core/src/view/provider';
+
 @Component({
   selector: 'app-clients',
   templateUrl: './clients.component.html',
   styleUrls: ['./clients.component.css']
 })
 export class ClientsComponent implements OnInit {
-  client: Client = {
-    id: 1,
-    name: 'SandStorm'
-  }
+  clients = CLIENTS;
+
+  selectedClient: Client;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelect(client: Client): void {
+    this.selectedClient = client;
   }
 
 }
